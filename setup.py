79351e5962fb5a26e920ac2a166e6b07085f9226
charu.gera <gera.charu@gmail.com>
2021-07-09 16:34:01 +0100

    First Commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A model to predict loan default',
+    author='Charu Gera',
+    license='MIT',
+)
